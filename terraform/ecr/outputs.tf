c7cc5f9a68342d39a74a1b9386e62ec0ca8aa940
Kevin Meyer <meyerkev248@gmai.com>
2025-05-28 22:39:06 -0400

    Install tfenv

--- terraform/ecr/outputs.tf
+++ terraform/ecr/outputs.tf
@@ -6,6 +6,10 @@ output "github_actions_role_arn" {
   value = aws_iam_role.github_actions_role.arn
 }
 
+output "github_actions_role_terraform_arn" {
+  value = aws_iam_role.github_actions_role_terraform.arn
+}
+
 output "thumbprints" {
   value = [for cert in data.tls_certificate.github_oidc.certificates : cert.sha1_fingerprint ]
 }
