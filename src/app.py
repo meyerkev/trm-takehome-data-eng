a0f07197bf73ff6ba0955e65bdbb51ff144a3fe9
Kevin Meyer <meyerkev248@gmai.com>
2025-05-28 21:25:53 -0400

    Debugged Helm and also added the balance API call

--- src/app.py
+++ src/app.py
@@ -1,5 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
+import requests
+import os
+import logging
+
 app = Flask(__name__)
+
+# Add a logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
 
 @app.route('/address/exposure/direct',  methods=['GET'])
 def address_exposure_direct():
@@ -26,5 +35,71 @@ def address_exposure_direct():
     }
 
   return res
+
+# From the docs:
+# https://docs.infura.io/infura/networks/ethereum/json-rpc-api/eth_getbalance
+'''
+curl https://mainnet.infura.io/v3/<YOUR-API-KEY> \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"jsonrpc": "2.0","method": "eth_getBalance", "params": ["address", "latest"], "id": 1}'
+'''
+@app.route('/address/balance/<address>')
+def address_balance(address):
+  try:
+    # Get the INFURA_API_KEY from environment variables
+    infura_api_key = os.environ.get('INFURA_API_KEY')
+    if not infura_api_key:
+      return {
+        "data": {},
+        "success": False,
+        "error": "INFURA_API_KEY not found"
+      }
+
+    # Make the API call to Infura
+    url = f"https://mainnet.infura.io/v3/{infura_api_key}"
+    headers = {"Content-Type": "application/json"}
+    payload = {
+      "jsonrpc": "2.0",
+      "method": "eth_getBalance",
+      "params": [address, "latest"],
+      "id": 1
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    response_data = response.json()
+
+    if response.status_code == 200 and 'result' in response_data:
+      # Convert hex balance to decimal (Wei)
+      balance_wei = int(response_data['result'], 16)
+      # Convert Wei to ETH (1 ETH = 10^18 Wei)
+      balance_eth = balance_wei / 10**18
+
+      res = {
+        "balance": balance_eth
+      }
+    else:
+      res = {
+        "data": {},
+        "success": False,
+        "error": response_data.get('error', 'Unknown error')
+      }
+
+  except Exception as e:
+    res = {
+      "data": {},
+      "success": False,
+      "error": str(e)
+    }
+
+  return res
+
+@app.route('/')
+def index():
+  # Redirect to /address/exposure/direct
+  return redirect(url_for('address_exposure_direct'))
+
+
+
 
 # vim: ts=2 sts=2 sw=2 et
